3c582d5387c37b467e01c4fab4104a97c6ba5a95
Yomawa <bogeski.maja@gmail.com>
2015-11-25 15:10:26 -0800

    chek out second page

--- js/services.js
+++ js/services.js
@@ -8,6 +8,16 @@ app.service("orderService",function(){
     addItem: function(tea){
       // for(var i=0; i<tea.qty; i++)
       // while(tea.qty--){
+        //if string !== "string"
+        if(typeof(tea.qty) !== "string") {
+          tea.qty = 1;
+        }
+        for(var i=0; i<items.length; i++) {
+          if(items[i]._id === tea._id) {
+            items.splice(i,1,tea);
+            return;  //if there is a match will never get to line 21, return means exit or retruning nothing your are done         
+          }
+        }
         items.push(tea);
       // }
     },
